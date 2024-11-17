5a9772a13f5b3f7aa6b6c64fe0e01630c8593855
SeimicHazard <55723255+fmc202@users.noreply.github.com>
2024-11-16 19:25:07 -0500

    Update PGV_CGMM_SUBUDCTION.py

--- PGV_CGMM_SUBUDCTION.py
+++ PGV_CGMM_SUBUDCTION.py
@@ -50,38 +50,8 @@ def con_pgv_crustal(M, Rrup, Vs30, lnSa_Tpgv, tra_sigma=None):
     return lnPGV, sigma
 
 
+
 def con_pgv_sub(M, Rrup, Vs30, lnSa_Tpgv, mechanism, tra_sigma=None):
-    # PGV output in cm/s
-    # Sa_Tpgv in g
-    # mechanism: interface =0; intraslab = 1
-    # tra_sigma: sigma of lnSa_Tpgv
-
-    F = mechanism
-    a = np.array([[5.09, 0.63, 0.67, 0.82, -0.04, -0.5, -0.29, 0.09, 0.22, 0.33], [5.22, 0.77, 0.71, 0.72, 0.02, -0.55, -0.09, -0.01, 0.21, 0.31]])
-    sigma_con = np.sqrt(a[F, -2] ** 2 + a[F, -1] ** 2)
-
-    # median model
-    f_mag = (M < 5) * a[F, 1] + (M >= 5) * (M <= 7.5) * (a[F, 1] + (a[F, 2] - a[F, 1]) * (M - 5.5) / 2.5) + (M > 7.5) * a[F, 2]
-
-    lnPGV = (
-        a[F, 0]
-        + f_mag * lnSa_Tpgv
-        + a[F, 3] * (M - 6)
-        + a[F, 4] * (8.5 - M) ** 2
-        + a[F, 5] * np.log(Rrup + 5 * np.exp(0.4 * (M - 6)))
-        + (a[F, 6] + a[F, 7] * (M - 5)) * np.log(Vs30 / 425)
-    )
-
-    # propagation of errors
-    if tra_sigma is not None:
-        sigma = np.sqrt(sigma_con ** 2 + (f_mag ** 2) * (tra_sigma ** 2))
-    else:
-        sigma = None
-
-    return lnPGV, sigma
-
-
-def alternative_form(M, Rrup, Vs30, lnSa_Tpgv, mechanism, tra_sigma=None):
     # this should be equivalent to con_pgv_sub but with different centering M and Vs30 values
     # Try use con_pgv_sub instead
 
